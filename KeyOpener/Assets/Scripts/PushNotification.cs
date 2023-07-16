506678d3f9eea68492925f01a014e4fa9f03e5b8
patryczektak <patryczektak@gmail.com>
2023-07-16 17:51:00 +0200

    services firebase/play

--- KeyOpener/Assets/Scripts/PushNotification.cs
+++ KeyOpener/Assets/Scripts/PushNotification.cs
@@ -1,12 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Firebase.Messaging;
+using Firebase.Extensions;
 
 public class PushNotification : MonoBehaviour
 {
     //Start is called before the first frame update
     public void Start()
     {
+        Firebase.FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task => {
+            var dependencyStatus = task.Result;
+            if (dependencyStatus == Firebase.DependencyStatus.Available)
+            {
+                // Create and hold a reference to your FirebaseApp,
+                // where app is a Firebase.FirebaseApp property of your application class.
+                Firebase.FirebaseApp app = Firebase.FirebaseApp.DefaultInstance;
+
+                // Set a flag here to indicate whether Firebase is ready to use by your app.
+            }
+            else
+            {
+                UnityEngine.Debug.LogError(System.String.Format(
+                  "Could not resolve all Firebase dependencies: {0}", dependencyStatus));
+                // Firebase Unity SDK is not safe to use here.
+            }
+        });
+
+
         Firebase.Messaging.FirebaseMessaging.TokenReceived += OnTokenReceived;
         Firebase.Messaging.FirebaseMessaging.MessageReceived += OnMessageReceived;
     }
@@ -21,9 +42,4 @@ public class PushNotification : MonoBehaviour
         UnityEngine.Debug.Log("Received a new message from: " + e.Message.From);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
